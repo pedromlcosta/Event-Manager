2bd84b26b1d2e63962e9fb2a3901344553168b95
ruiProg <ruifilipeosorio17@hotmail.com>
2015-11-28 22:00:30 +0000

    tab proto

---
+++ templates/filter.php
@@ -0,0 +1,16 @@
+<div id="filter">
+	<form action="action_filter.php" method="post">
+		<div class="sortSelection">
+			<label><input type="radio" name="sortType" value="Date" checked>Date</label>
+			<label><input type="radio" name="sortType" value="Popularity">Popularity</label>
+		</div>
+		<div class="types">
+			<label><input type="checkbox" name="type" value="Party" checked> Party</label>
+			<label><input type="checkbox" name="type" value="Concert" checked> Concert</label>
+			<label><input type="checkbox" name="type" value="Conference" checked> Conference</label>
+			<label><input type="checkbox" name="type" value="Meeting" checked> Meeting</label>
+			<label><input type="checkbox" name="type" value="Wedding" checked> Wedding</label>
+			<label><input type="checkbox" name="type" value="Birthday" checked> Birthday</label>
+			<label><input type="checkbox" name="type" value="Sports" checked> Sports</label>
+			<label><input type="checkbox" name="type" value="Fundraising" checked> Fundraising</label>
+		</div>
