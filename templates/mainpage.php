5332a917289a962c928a8b1be848e7988236b033
ruiProg <ruifilipeosorio17@hotmail.com>
2015-11-29 18:20:42 +0000

    tabs diff between log state

--- templates/mainpage.php
+++ templates/mainpage.php
@@ -1,38 +1,42 @@
 <div id="mainPage">
 	<ul id="tabs">
+		<?php if(isLogged()): ?>
 		<li><a id="link_myEvents" href="#myEvents">My Events</a></li>
 		<li><a id="link_hostingEvents" href="#hostingEvents">Hosting Events</a></li>
 		<li><a id="link_invitedEvents" href="#invitedEvents">Invitations</a></li>
 		<li><a id="link_otherEvents" href="#otherEvents">Other Events</a></li>
 		<li><a id="link_customSearch" href="#customSearch" >Custom Search</a></li>
-	</ul>
-	<div id="myEvents" class="tab-section">
-		<?php include('filter.php'); ?>
-		<?php include('submit.php'); ?>
-	</div>
-	<div id="hostingEvents" class="tab-section">
-		<?php include('filter.php'); ?>
-		<?php include('submit.php'); ?>
-	</div>
-	<div id="invitedEvents" class="tab-section">
-		<?php include('filter.php'); ?>
-		<?php include('submit.php'); ?>
-	</div>
-	<div id="otherEvents" class="tab-section">
-		<?php include('filter.php'); ?>
-		<?php include('submit.php'); ?>
-	</div>
-	<div id="customSearch" class="tab-section">
-		<?php include('filter.php'); ?>
-		<?php include('customSearch.php'); ?>
-		<?php include('submit.php'); ?>
-	</div>
-	<ul id="event_list">
-		<?php
-			$events = getAllEvents();
-			for($i = 0; $i < count($events); $i++){
-				echo "<li>".$events[$i]['title'] ."</li>";
-			}
-		?>
-	</ul>
+	<?php else: ?>
+	<li><a id="link_customSearch" href="#customSearch" >Custom Search</a></li>
+<?php endif; ?>
+</ul>
+<div id="myEvents" class="tab-section">
+	<?php include('filter.php'); ?>
+	<?php include('submit.php'); ?>
+</div>
+<div id="hostingEvents" class="tab-section">
+	<?php include('filter.php'); ?>
+	<?php include('submit.php'); ?>
+</div>
+<div id="invitedEvents" class="tab-section">
+	<?php include('filter.php'); ?>
+	<?php include('submit.php'); ?>
+</div>
+<div id="otherEvents" class="tab-section">
+	<?php include('filter.php'); ?>
+	<?php include('submit.php'); ?>
+</div>
+<div id="customSearch" class="tab-section">
+	<?php include('filter.php'); ?>
+	<?php include('customSearch.php'); ?>
+	<?php include('submit.php'); ?>
+</div>
+<ul id="event_list">
+	<?php
+	$events = getAllEvents();
+	for($i = 0; $i < count($events); $i++){
+		echo "<li>".$events[$i]['title'] ."</li>";
+	}
+	?>
+</ul>
 </div>
