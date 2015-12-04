81478d1ce492e9ba81050ec3a0ac31e65fe63b56
pedromlcosta <up201303973@fe.up.pt>
2015-12-04 20:14:57 +0000

    Mah derp was real

--- user_page.php
+++ user_page.php
@@ -2,9 +2,10 @@
 	include_once('init.php'); // connects to the database
 	include_once('database/users.php');
 	include_once('database/events.php');
+	include('genericStart.html');
 ?>
 
-<?
+<?php
 	include('templates/header.php');
  	include('templates/user_page.php');      
   	include('templates/footer.php');
