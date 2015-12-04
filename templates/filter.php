acbccd15b8338db1a432f7a0473e9abd68a5fc34
pedromlcosta <up201303973@fe.up.pt>
2015-12-04 19:59:56 +0000

    Moar stuffs

--- templates/filter.php
+++ templates/filter.php
@@ -1,15 +1,17 @@
 <div id="filter">
 		<div class="sortSelection">
-			<label><input type="radio" name="sortType" value="Date" checked>Date</label>
+			<h4 class="selTitle">Sort By:</h4>
+			<label><input type="radio" name="sortType" value="Date" checked="checked">Date</label>
 			<label><input type="radio" name="sortType" value="Popularity">Popularity</label>
 			<label><input type="radio" name="sortType" value="Type">Type</label>
 		</div>
-		<div class="types">
+		<div class="typeSelection">
+			<h4 class="selTitle">Filters:</h4>
 			<?php
 				$types = getTypes();
 
 				for($i = 0; $i < count($types); $i++){
-					echo ("<label><input type=\"checkbox\" name=\"type\" value=\" $types[$i] \" checked> $types[$i] </label>");
+					echo ("<label><input type=\"checkbox\" name=\"type\" value=\"$types[$i]\" checked=\"checked\"> $types[$i] </label>");
 				}
 			?>
 			
