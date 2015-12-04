dd2b5ad017358180db67c850e76097ac0f3c91c7
Ncf16 <joaofilipe26@gmail.com>
2015-12-04 21:19:33 +0000

    useless function

--- database/image.php
+++ database/image.php
@@ -6,12 +6,7 @@ function getImage($id){
   $stmt->execute(array($id));  
   return $stmt->fetch();
 }
-function getImageByPath($image){
-   global $db;
-  $stmt = $db->prepare('SELECT  * FROM images WHERE visible=1 AND image= ?');
-  $stmt->execute(array($image));  
-  return $stmt->fetch();
-}
+
 function getLastimageId(){
    global $db;
   $stmt = $db->prepare('SELECT MAX(id) as id  FROM  images WHERE visible=1 ');
