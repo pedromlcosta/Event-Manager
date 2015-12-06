67bec04fb0cb8d20388abeb5d32e9ef78735f166
Ncf16 <joaofilipe26@gmail.com>
2015-12-06 20:31:23 +0000

    fix

--- action_createEvents.php
+++ action_createEvents.php
@@ -8,9 +8,10 @@ include_once('database/tag.php');
 include_once('database/tagEvent.php');
 include_once('database/usersEvent.php');
 
- 
-
-if (isset($_SESSION['userID']) && isset($_POST['title']) && isset($_POST['fullText']) && isset($_POST['data']) && isset($_POST['Event_Type']) && isLogged()) {
+ if(isset($_SESSION['errors'])){
+    unset($_SESSION['errors']);
+}
+else if (isset($_SESSION['userID']) && isset($_POST['title']) && isset($_POST['fullText']) && isset($_POST['data']) && isset($_POST['Event_Type']) && isLogged()) {
     
 	$errorMessage = '';
 
@@ -83,8 +84,7 @@ if (isset($_SESSION['userID']) && isset($_POST['title']) && isset($_POST['fullTe
         createImageEvent($eventCreatedId['id'],$imageId);*/
     }
     else{
-        echo "ERROR IN EVENT INPUT <br>";
-        var_dump($errorMessage);
+       $_SESSION['errors']=$errorMessage;
     }
 }
 
